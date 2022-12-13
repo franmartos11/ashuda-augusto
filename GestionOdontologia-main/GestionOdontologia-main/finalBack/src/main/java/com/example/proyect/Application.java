2cf199288ec9d5efbddacd7e6276044c27f9e2fc
franmartos11 <franmartos11@gmail.com>
2022-12-13 03:24:00 -0300

    m

--- GestionOdontologia-main/GestionOdontologia-main/finalBack/src/main/java/com/example/proyect/Application.java
+++ GestionOdontologia-main/GestionOdontologia-main/finalBack/src/main/java/com/example/proyect/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class ProyectoIntegradorApplication {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoIntegradorApplication.class, args);
